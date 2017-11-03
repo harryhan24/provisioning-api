c9dcedfaa772835942957aadd71cbcdc05250366
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2017-11-03 13:42:34 +1000

    Add full test coverage for the ApiUser Middleware [PT #152532918]

--- src/http/middleware/apiUser.js
+++ src/http/middleware/apiUser.js
@@ -7,8 +7,18 @@ export default async (req: $Request, res: $Response, next: () => void) => {
   if (req.originalUrl.startsWith("/sp/")) {
     next();
   } else {
-    // Do a check on the API user
+    // Check if we have received an authorization header
+    if (
+      req.headers.authorization === undefined ||
+      req.headers.authorization.includes("ApiKey") === false
+    ) {
+      return res.status(403).json({
+        status: 403,
+        message: "No authorization headers provided",
+      });
+    }
 
+    // Check if we received a valid Authorization header
     const apiKey = req.headers.authorization.replace("ApiKey ", "").trim();
     if (apiKey === "") {
       return res.status(403).json({
@@ -29,7 +39,7 @@ export default async (req: $Request, res: $Response, next: () => void) => {
       res.locals.apiUser = apiUser;
       next();
     } catch (e) {
-      return res.status(400).send(e);
+      return res.status(400).send({ status: 400, e });
     }
   }
 };
