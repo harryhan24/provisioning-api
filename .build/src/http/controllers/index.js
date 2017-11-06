c81ce8de137bd588f8e304e4c24a89a15cb0ad31
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2017-11-06 15:02:38 +1000

    Remove build directory files

--- .build/src/http/controllers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var lookupController_1 = require("./lookupController");
-exports.lookupController = lookupController_1.default;
-var projectController_1 = require("./projectController");
-exports.projectController = projectController_1.default;
-var serviceProviderController_1 = require("./serviceProviderController");
-exports.serviceProviderController = serviceProviderController_1.default;
-//# sourceMappingURL=index.js.map
--- .build/src/utils/validation.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidShortCode = (shortCode) => typeof shortCode === "string" && /^([a-zA-Z0-9]{6,10})$/.test(shortCode);
-//# sourceMappingURL=validation.js.map
